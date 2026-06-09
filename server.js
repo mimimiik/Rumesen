71f5f5d49bb45ba194f89e688ae93813005cc219
mimimiik <wada747479@gmail.com>
2026-06-09 03:45:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Обслуживаем статические файлы из текущей директории
 app.use(express.static(__dirname));
 
+// Обрабатываем все входящие запросы
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'messenger.html'));
+    // Проверяем, какой файл существует в папке проекта, и отдаем его
+    const indexPath = path.join(__dirname, 'index.html');
+    const messengerPath = path.join(__dirname, 'messenger.html');
+    const publicIndexPath = path.join(__dirname, 'public', 'index.html');
+    const publicMessengerPath = path.join(__dirname, 'public', 'messenger.html');
+
+    if (fs.existsSync(indexPath)) {
+        res.sendFile(indexPath);
+    } else if (fs.existsSync(messengerPath)) {
+        res.sendFile(messengerPath);
+    } else if (fs.existsSync(publicIndexPath)) {
+        res.sendFile(publicIndexPath);
+    } else if (fs.existsSync(publicMessengerPath)) {
+        res.sendFile(publicMessengerPath);
+    } else {
+        res.status(404).send('Ошибка: Главный HTML-файл мессенджера не найден в проекте.');
+    }
 });
 
 app.listen(PORT, () => {
