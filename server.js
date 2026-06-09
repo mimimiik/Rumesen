5ed86972562d92ecc33f9859fd012d9e760c7d6d
mimimiik <wada747479@gmail.com>
2026-06-09 03:38:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-```javascript
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Отдаем файлы из текущей директории
 app.use(express.static(__dirname));
 
-// Любой другой запрос перенаправляем на файл мессенджера
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'messenger.html'));
 });
